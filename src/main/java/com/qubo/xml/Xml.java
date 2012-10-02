9a67b13c78635b2841fa979851b687cca1f2199a
tk.qubo <tk.qubo@gmail.com>
2012-10-03 00:10:02 +0900

    Formatting introduced into Xml.java

--- src/main/java/com/qubo/xml/Xml.java
+++ src/main/java/com/qubo/xml/Xml.java
@@ -3,6 +3,8 @@ package com.qubo.xml;
 import java.util.Map.Entry;
 
 public class Xml extends AbstractParentOf<ChildOf<Xml>, Xml> {
+	private static final String XML_INDENT = "\t";
+
 	public Xml(String name) {
 		super(name);
 	}
@@ -14,11 +16,14 @@ public class Xml extends AbstractParentOf<ChildOf<Xml>, Xml> {
 
 	public void build() {
 		StringBuilder builder = new StringBuilder();
-		build(builder, this);
+		build(builder, this, 0);
 		System.out.println(builder);
 	}
 
-	private <T> void build(StringBuilder builder, ParentOf<T> parent) {
+	private <T> void build(StringBuilder builder, ParentOf<T> parent, int indent) {
+		for (int i = 0; i < indent; i++) {
+			builder.append(XML_INDENT);
+		}
 		builder.append("<").append(parent.name());
 		for (Entry<String, String> entry : parent.as()) {
 			builder.append(" ")
@@ -28,13 +33,15 @@ public class Xml extends AbstractParentOf<ChildOf<Xml>, Xml> {
 					.append(entry.getValue())
 					.append("\"");
 		}
-		builder.append(">");
+		builder.append(">\n");
 		for (T child : parent._s()) {
 			if (child instanceof ParentOf<?>) {
-				build(builder, (ParentOf<?>) child);
+				build(builder, (ParentOf<?>) child, indent + 1);
 			}
 		}
-		builder.append("</").append(parent.name()).append(">");
-
+		for (int i = 0; i < indent; i++) {
+			builder.append(XML_INDENT);
+		}
+		builder.append("</").append(parent.name()).append(">\n");
 	}
 }
